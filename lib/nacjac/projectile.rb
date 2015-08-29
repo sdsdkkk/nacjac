b7b1026fa640975c7a6f6ca9198523732304d9c1
sdsdkkk <vcc.edwint@gmail.com>
2015-08-29 21:47:37 +0700

    fix typo, remove unnecessary piece of code

--- lib/nacjac/projectile.rb
+++ lib/nacjac/projectile.rb
@@ -7,10 +7,7 @@ module Nacjac
 
     def initialize(target_url)
       @target_url = target_url
-      @conn = Faraday.new(:url => target_url) do |faraday|
-        faraday.request  :url_encoded
-        faraday.adapter  Faraday.default_adapter
-      end
+      @conn = Faraday.new
     end
 
     def launch
